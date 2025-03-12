2e2b52f64654c8f7b442e11d92bb4160bbd445f4
Maxim D <renigano@gmail.com>
2025-03-12 10:41:06 +0300

    feat:blogs controller

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -8,7 +8,14 @@ const postController = {
     getAllBlogs(req: Request, res: Response,) {
         const blogs = blogsRepository.getAllBlogs()
         res.status(200).json(blogs).end()
+        return
+    },
+    getBlogById(req: Request, res: Response,) {
+        const blog = blogsRepository.findBlog(req.params.id)
+        blog ? res.status(200).json(blog).end() : res.status(404).end()
+        return
     }
 }
 
 blogsRouter.get('/', postController.getAllBlogs)
+blogsRouter.get('/:id', postController.getBlogById)
--- src/blogs/blogs.repository.ts
+++ src/blogs/blogs.repository.ts
@@ -3,5 +3,9 @@ import {db} from "../db/db";
 export const blogsRepository = {
     getAllBlogs() {
         return db.blogs;
+    },
+    findBlog(id: string) {
+        return db.blogs.find(el => el.id === id
+        );
     }
 }
