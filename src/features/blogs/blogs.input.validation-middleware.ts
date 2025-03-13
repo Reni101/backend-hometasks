15efaea607edd593de8d2be2873848cdd11bfce0
Maxim D <renigano@gmail.com>
2025-03-13 22:03:14 +0300

    fix:validation

--- src/features/blogs/blogs.input.validation-middleware.ts
+++ src/features/blogs/blogs.input.validation-middleware.ts
@@ -1,8 +1,8 @@
 import {body} from "express-validator";
 
-const name = body('name').isString().trim().isLength({max: 15})
-const description = body('description').isString().trim().isLength({max: 500})
-const websiteUrl = body('websiteUrl').isString().trim().isLength({max: 100}).isURL()
+const name = body('name').isString().trim().isLength({min: 1,max: 15})
+const description = body('description').isString().trim().isLength({min: 1,max: 500})
+const websiteUrl = body('websiteUrl').isString().trim().isLength({min: 1,max: 100}).isURL()
 
     // .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
 
--- src/features/posts/posts.input.validation-middleware.ts
+++ src/features/posts/posts.input.validation-middleware.ts
@@ -1,10 +1,10 @@
 import {body} from "express-validator";
 
 
-const title = body('title').isString().trim().isLength({max: 30})
-const shortDescription = body('shortDescription').isString().trim().isLength({max: 100})
-const content = body('content').isString().trim().isLength({max: 1000})
+const title = body('title').isString().isLength({min: 1, max: 30})
+const shortDescription = body('shortDescription').isString().trim().isLength({min: 1,max: 100})
+const content = body('content').isString().trim().isLength({min: 1,max: 1000})
 const blogId = body('blogId').isString().trim()
 
 
-export const postInputValidation = [title,shortDescription,content,blogId]
+export const postInputValidation = [title, shortDescription, content, blogId]
