9f2ebbeca3734069509b011156515a6cdbba1b87
Maxim D <renigano@gmail.com>
2025-03-13 21:58:34 +0300

    fix:trim

--- src/features/posts/posts.input.validation-middleware.ts
+++ src/features/posts/posts.input.validation-middleware.ts
@@ -1,10 +1,10 @@
 import {body} from "express-validator";
 
 
-const title = body('title').isString().isLength({max: 30})
-const shortDescription = body('shortDescription').isString().isLength({max: 100})
-const content = body('content').isString().isLength({max: 1000})
-const blogId = body('blogId').isString()
+const title = body('title').isString().trim().isLength({max: 30})
+const shortDescription = body('shortDescription').isString().trim().isLength({max: 100})
+const content = body('content').isString().trim().isLength({max: 1000})
+const blogId = body('blogId').isString().trim()
 
 
 export const postInputValidation = [title,shortDescription,content,blogId]
