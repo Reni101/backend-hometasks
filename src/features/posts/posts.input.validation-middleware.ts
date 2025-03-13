51afd6838eab36c10a903b36ed03beca0ef0407c
Maxim D <renigano@gmail.com>
2025-03-13 23:10:48 +0300

    fix:blog validation

--- src/features/posts/posts.input.validation-middleware.ts
+++ src/features/posts/posts.input.validation-middleware.ts
@@ -1,10 +1,14 @@
 import {body} from "express-validator";
+import {blogsRepository} from "../blogs/blogs.repository";
 
 
 const title = body('title').isString().trim().isLength({min: 1, max: 30})
 const shortDescription = body('shortDescription').isString().trim().isLength({min: 1,max: 100})
 const content = body('content').isString().trim().isLength({min: 1,max: 1000})
-const blogId = body('blogId').isString().trim()
+const blogId = body('blogId').isString().custom((blogId:string)=>{
+    const blog = blogsRepository.findBlog(blogId)
+    return !!blog
+})
 
 
 export const postInputValidation = [title, shortDescription, content, blogId]
--- src/middleware/errorsMiddleware.ts
+++ src/middleware/errorsMiddleware.ts
@@ -4,7 +4,7 @@ import {NextFunction, Request, Response} from "express";
 export const errorsMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const e = validationResult(req)
     if (!e.isEmpty()) {
-        const eArray = e.array() as { path: string, msg: string }[]
+        const eArray = e.array({onlyFirstError: true}) as { path: string, msg: string }[]
 
         res.status(400).json({
             errorsMessages: eArray.map(x => ({field: x.path, message: x.msg}))
