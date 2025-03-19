fdd352a5d851c8e0d9f76aa636ee4c3ebcfdb6f8
Maxim D <renigano@gmail.com>
2025-03-19 23:56:34 +0300

    fix: post id validation

--- src/features/posts/posts.input.validation-middleware.ts
+++ src/features/posts/posts.input.validation-middleware.ts
@@ -5,9 +5,9 @@ import {blogsRepository} from "../blogs/blogs.repository";
 const title = body('title').isString().trim().isLength({min: 1, max: 30})
 const shortDescription = body('shortDescription').isString().trim().isLength({min: 1,max: 100})
 const content = body('content').isString().trim().isLength({min: 1,max: 1000})
-const blogId = body('blogId').isString().custom((blogId:string)=>{
-    const blog = blogsRepository.findBlog(blogId)
-    return !!blog
-})
+// const blogId = body('blogId').isString().custom((blogId:string)=>{
+//     const blog = blogsRepository.findBlog(blogId)
+//     return !!blog
+// })
 
-export const postInputValidation = [title, shortDescription, content, blogId]
+export const postInputValidation = [title, shortDescription, content,]
