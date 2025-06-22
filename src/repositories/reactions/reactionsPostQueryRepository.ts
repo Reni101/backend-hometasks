cfc7baeb876c81125f6a4b458a9bc28530dd998a
Maxim D <renigano@gmail.com>
2025-06-22 20:32:54 +0300

    fix: new like logic

--- src/repositories/reactions/reactionsPostQueryRepository.ts
+++ src/repositories/reactions/reactionsPostQueryRepository.ts
@@ -5,11 +5,14 @@ import {ObjectId, WithId} from "mongodb";
 @injectable()
 export class ReactionsPostQueryRepository {
     async findNewReactions(postId: ObjectId) {
-        const reactions = await reactionPostModel.find({postId: postId}).sort({createdAt: -1}).limit(3).exec()
+        const reactions = await reactionPostModel.find({
+            postId: postId,
+            status: 'Like'
+        }).sort({createdAt: -1}).limit(3).exec()
         return reactions.map(this._map);
     }
 
     _map(el: WithId<ReactionPostType>) {
-        return {addedAt: el.createdAt, userId: el.userId, login: el.login,}
+        return {addedAt: el.createdAt, userId: el.userId, login: el.login}
     }
 }
